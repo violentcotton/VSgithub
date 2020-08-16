39381d7116f50fd3946b7864be76474d8dbc6de5
陈方俊 <violentcotton@163.com>
2020-08-16 22:27:12 +0800

    ISP+DI

---
+++ ISP/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num1 = { 1, 2, 3, 4, 5 };
+            ArrayList num2 = new ArrayList { 1, 2, 3, 4, 5 };
+            var roc = new ReadOnlyCollection(num1);
+            Console.WriteLine(Sum(num1));
+            Console.WriteLine(Sum(num2));
+            Console.WriteLine(Sum(roc));
+        }
+        static int Sum(IEnumerable nums)
+        {
+            int sum = 0;
+            foreach (var n in nums)
+            {
+                sum += (int)n;
+            }
+
+            return sum;
+        }
+    }
+    class ReadOnlyCollection : IEnumerable
+    {
+        private int[] _array;
+
+        public ReadOnlyCollection(int[] array)
+        {
+            _array = array;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        public class Enumerator : IEnumerator
+        {
+
+            private ReadOnlyCollection _collection;
+            private int _head;
+
+            public Enumerator(ReadOnlyCollection collection)
+            {
+                _collection = collection;
+                _head = -1;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    object o = _collection._array[_head];
+                    return o;
+                }
+            }
+
+            public bool MoveNext()
+            {
+                if (++_head<_collection._array.Length)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public void Reset()
+            {
+                _head = -1;
+            }
+        }
+    }
+}
